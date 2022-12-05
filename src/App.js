3513d654ace5dfdeae380902f0c654110198b144
emirdmrgzr <emir.demirgezer@hotmail.com>
2022-12-05 14:50:45 +0300

    commit1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { useState }from "react";
 
+function getRandomAnimal() {
+    const animals = ["cat ","horse ","dog ","cow ","bird ","gator "];
 
+    return animals[Math.floor(Math.random() * animals.length)]
+}
 
 function App(){
-    const[count, setCount] = useState(0);  // The first one we enter is our index variable, the second index we enter works as a function. As we use in the clickListen function.
+    // The first one we enter is our index variable, the second index we enter works as a function. As we use in the clickListen function.
     // We are using useState function if we are getting input from the user. 
     // Sometimes it maybe clicking something or inserting something. By this code we're supporting the return part.
 
+    const [animals, setAnimals] = useState([]);
+
     function clickListen(){
-        setCount(count+1); 
+        // modifies a piece of state
+        setAnimals([...animals,getRandomAnimal()]);
     };
 
     return (
         <div>
             <button onClick={clickListen}>Add Animal</button>
-            <div>Number of animals : {count}</div>
+            <div>{animals}</div>
         </div>
     );
 }
