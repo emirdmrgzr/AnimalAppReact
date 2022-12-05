f41e8e23640911779337a2984b28331fbc47cd10
emirdmrgzr <emir.demirgezer@hotmail.com>
2022-12-05 17:35:17 +0300

    ProblemSolved

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import { useState }from "react";
-import AnimalShow from "./AnimalShow";
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
 
 function getRandomAnimal() {
-    const animals = ["cat ","horse ","dog ","cow ","bird ","gator "];
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
 
-    return animals[Math.floor(Math.random() * animals.length)]
+  return animals[Math.floor(Math.random() * animals.length)];
 }
 
-function App(){
-    // The first one we enter is our index variable, the second index we enter works as a function. As we use in the clickListen function.
-    // We are using useState function if we are getting input from the user. 
-    // Sometimes it maybe clicking something or inserting something. By this code we're supporting the return part.
+function App() {
+// The first one we enter is our index variable, the second index we enter works as a function. As we use in the clickListen function.
+// We are using useState function if we are getting input from the user. 
+// Sometimes it maybe clicking something or inserting something. By this code we're supporting the return part.
 
-    const [animals, setAnimals] = useState([]);
+  const [animals, setAnimals] = useState([]);
 
-    function clickListen(){
-        // modifies a piece of state
-        setAnimals([...animals,getRandomAnimal()]);
-    };
+  function clickListen(){
+    // modifies a piece of state
+    setAnimals([...animals,getRandomAnimal()]);
+};
 
-    const renderedAnimals = animals.map((animal,index) =>{
-        return <AnimalShow type={animal}/>;
-    })
-;
-    return (
-        <div>
-            <button onClick={clickListen}>Add Animal</button>
-            <div>{renderedAnimals}</div>
-        </div>
-    );
+  const renderedAnimals = animals.map((animal,index) =>{
+    return <AnimalShow type={animal}/>;
+});
+
+  return (
+    <div>
+      <button onClick={clickListen}>Add Animal</button>
+      <div>{renderedAnimals}</div>
+    </div>
+  );
 }
-
 
 export default App;
